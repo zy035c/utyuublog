4524348df9ab5fec004a91f98e67ac286e51f3d3
Chen Ziyi <zy035c@gmail.com>
2023-12-20 01:16:05 +0800

    utyuu activaaated!!!!

---
+++ celestial/src/main/java/com/laptisis/celestial/CelestialApplication.java
@@ -0,0 +1,13 @@
+package com.laptisis.celestial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelestialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelestialApplication.class, args);
+	}
+
+}
---
+++ celestial/src/test/java/com/laptisis/celestial/CelestialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laptisis.celestial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelestialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
